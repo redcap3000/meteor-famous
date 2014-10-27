645eebfea6a509c339b67ff10a4b33fcaa6be51f
Ronaldo Barbachano <redcap3000@gmail.com>
2014-10-26 23:03:16 -0700

    some formatting changes within famous, code cleanup, aded fade in transitionable to grid, more notes

--- client/lib/grid-example.js
+++ client/lib/grid-example.js
@@ -1,10 +1,11 @@
 
 /*
 Ronaldo Barbachano
-Oct. 2014
+Oct. 25 2014
 
 Example of how to setup a grid structure inside of a page
-view layout within Meteor.js.
+view layout within Meteor.js. Combines concepts several of the online lessons to
+function in a reactive meteor enviornment. Learn with me!
 
 Supports removing/adding reactively!
 
@@ -16,9 +17,17 @@ Using mjn:famous 0.3.0_5
 
 How this works!
 
-1 ) We retreve data from a dummy template dataset function (sampleSet)
+1 ) Retreve data from a dummy template dataset function (sampleSet)
 2 ) String that inside of template multiSurface {{#each sampleSet}} 
 3 ) {{#each}} renders data with template {{>innerSurface}}
+4 ) Use created/rendered/destroyed functions to control when things get added to the context
+
+Forms
+
+Meteor's templates are tricky (even possible?) to map to famous so I've resorted to using
+onClick parameters in the html and a global function (and jquery to get the web page data.)
+Its a step backwards but it works! Also included is a way to inject a form's HTML into a surface using
+$('#div').html.
 
 Sessions
 
@@ -28,7 +37,8 @@ Sessions
 
 Subscriptions
 
-• This needs some work, but the destroy function sort of works around a known issue. But it is reactive!
+• Continually re-create the grid each time something changes; and use jquery to remove any dom elements
+  that may remain from past sets.
 
 */
 Meteor.startup(function(){
@@ -41,9 +51,82 @@ Meteor.startup(function(){
 	StateModifier = famous.modifiers.StateModifier,
 	GridLayout = famous.views.GridLayout;
 	HeaderFooterLayout = famous.views.HeaderFooterLayout,
-	Transform = famous.core.Transform;
-	Easing = famous.transitions.Easing;
+	Modifier = famous.core.Modifier,
+	Transform = famous.core.Transform,
+	Transitionable = famous.transitions.Transitionable,
 
+	Easing = famous.transitions.Easing;
+	// for the selectable items.. sets selected/unselected states - tracks it too
+	// via gridSelect Session key
+	makeInnerSurface = function(_id,name,content){
+		var defaultSelectedProp = {backgroundColor:"orange",color:"white",textAlign:"center"},
+			defaultProp = {backgroundColor:"black",color:"red",textAlign:"center"};
+		var s = new Surface({
+					size : [undefined,undefined],
+					properties : defaultProp,
+					content : content
+				}
+				);
+		if(s){
+			// storing these here because they lose scope in the on function
+			// may need to make one for id
+			s.on('click', function() {
+				console.log(_id);
+				// check for id.. force it to be a string just in case.. 
+				// most session vars are strings
+				if(!Session.equals("gridSelect",_id)){
+					// add a link to allow for item deletion?
+					this.setContent(content + '<button onClick="sampleSet.remove({_id :\''+ _id +'\'});">Remove</button>');
+					// background color check ...
+					if(this.properties.backgroundColor != "orange"){
+						Session.set("gridSelect",_id);
+						this.setProperties(defaultSelectedProp);
+					}
+					else{
+						this.setContent(name);
+						this.setProperties(defaultProp);
+						Session.set("gridSelect",undefined);
+					}
+				}else{
+					// restore 'defaults somehow...'
+					this.setContent(name);
+					this.setProperties(defaultProp);
+					// clear the currently selected grid
+					Session.set("gridSelect",undefined);
+				}
+			});
+			return s;
+		}
+		return false;
+	};
+	addRecord = function(){
+		// weird thing... bug workaround with getting the right input
+		var v = $(".rName");
+		var c = $(".rContent");
+		var r = {};
+		if(v && v.length > 0){
+			// get last element
+			var v = v[v.length-1];
+			if(v && typeof v.value != "undefined" && v.value != ''){
+				r.name = v.value
+				if(c && c.length > 0){
+					var c = c[c.length-1];
+					if(c.value != "undefined"){
+						r.content = c.value;
+					}
+				}
+				sampleSet.insert(r,function(err,result){
+					if(typeof err == "undefined"){
+						Template.multiSurface.rendered();
+					}
+				});
+			}else{
+				alert("Must at least have a name");
+			}
+		}else{
+			console.log('add record error');
+		}
+	};
 	// this is used to push values from innerSurface to be
 	// passed to .sequenceFrom for the grid
 	Meteor.subscribe("gridExample",function(err,result){
@@ -55,26 +138,26 @@ Meteor.startup(function(){
 			console.log('sub error');
 		}
 	});
-
 });
 
 Template.multiSurface.rendered = function(){
 	context = famous.core.Engine.createContext();
+
+	// make header footer layout structures
 	layout = new HeaderFooterLayout({
 			headerSize:100,
 			footerSize: 50
 		});
 
 	layout.header.add(new Surface({
-			content: "Header",
-			properties:{
-				backgroundColor: 'gray',
-				lineHeight: "100px",
-				textAlign: "center"
-			}
+		content: "Header",
+		properties:{
+			backgroundColor: 'gray',
+			lineHeight: "100px",
+			textAlign: "center"
+		}
 		}));
 	layout.content.add(new Surface({
-		
 		properties :{
 			backgroundColor :'white',
 			lineHeight: '400px',
@@ -92,96 +175,45 @@ Template.multiSurface.rendered = function(){
 		    }
 		}));
 	}
-		var fCon = $(".famous-container");
+	var fCon = $(".famous-container");
 	if(fCon.length > 1){
-		//console.log(fCon.length);
 		for (var i = 0; i <= fCon.length-2; i++) {
 			fCon[i].remove();
 
 		}
 	}
+	var modifier = new Modifier();
+	var opacityState = new Transitionable(0);
 	if( typeof context != "undefined" && typeof theSurfaces != "undefined"){
 		// code from code academy... sort of works but not too excited about it..
 		// need to do some on window change reloading...
-			var grid = new GridLayout({
-			    dimensions: [2, 8]
-			});
-			theSurfaces = [];
-			var mod = new StateModifier({
-		
-//				align: [0,1]
-			});
-
-			mod.setTransform(
-			    Transform.scale(.9, .9,.9),
-			    { duration : 1000, curve: Easing.outElastic }
-			);
-		
-			// so figure out if we are removing something and then change the transition and
-			// reset self.. eventually
-			grid.sequenceFrom(theSurfaces);
-			// it doesnt want to do this here??
-			sampleSet.find().map(function(o){
-				var s = new Surface({
-					size : [undefined,undefined],
-					opacity: 1,
-					properties : {color:"red",backgroundColor:"black"},
-					content : o.name
-				}
-				);
-				
+		var grid = new GridLayout({
+		    dimensions: [2, 4]
+		});
+		theSurfaces = [];
+		grid.sequenceFrom(theSurfaces);
+		sampleSet.find().map(function(o){
+			var s = makeInnerSurface(o._id,o.name,o.content);
+				// push surface for multiSurface.rendered
 				if(s){
-					// storing these here because they lose scope in the on function
-					var theContent = o.content, theName = o.name, theId = o._id;
-					// need this as global... this is sort of a 'band aid'
-					// may need to make one for id
-					s.on('click', function() {
-						// check for id.. force it to be a string just in case.. 
-						// most session vars are strings
-						if(!Session.equals("gridSelect",theId)){
-							Session.set("gridSelect",theId);
-							this.setContent(theContent + '<button onClick="sampleSet.remove({_id :\''+ theId +'\'});">Remove</button>');
-							// background color check ...
-							if(this.properties.backgroundColor != "orange")
-								this.setProperties({backgroundColor:"orange",color:"white"});
-							else{
-								this.setProperties({backgroundColor:"black",color:"red"});
-							}
-						}else{
-							// restore 'defaults somehow...'
-							this.setContent(theName);
-							this.setProperties({backgroundColor:"black",color:"red"});
-							// clear the currently selected grid
-							Session.set("gridSelect",undefined);
-						}
-					});
-					// push surface for multiSurface.rendered
 					s._id = o._id;
 					theSurfaces.push(s);
 				}else{
-					console.log('could not create surface');
+					console.log("could not make surface");
 				}
-			});
-
-	
-				
-		layout.content.add(mod).add(grid);
-		context.add(layout);
+		});
+		// modifier with transitonable	
+		layout.content.add(modifier).add(grid);
+		context.add(modifier).add(layout).add(modifier);
+		modifier.opacityFrom(opacityState);
+		opacityState.set(
+		    1,
+		    {curve : 'linear', duration : 500},
+		    function(){ console.log('animation finished!'); }
+		);
 	}else{
-		var mod = new StateModifier({
-		
-//				align: [0,1]
-			});
-
-			mod.setTransform(
-			    Transform.scale(1, 1,1),
-			    { duration : 1000, curve: Easing.inElastic }
-			);
-		context.add(mod).add(layout);
+		context.add(modifier).add(layout);
 	}
-	// check if any containers exist?
-
-
 };
 
 Template.multiSurface.helpers({
@@ -189,99 +221,29 @@ Template.multiSurface.helpers({
 		return sampleSet.find();
 	}
 });
-Template.addRecord.rendered = function () {
-	// setup global functions for adding records directly
-	// from famous dom
-	addRecord = function(){
-		// weird thing... bug workaround with getting the right input
-		var v = $(".rName");
-		var c = $(".rContent");
-		var r = {};
-		if(v && v.length > 0){
-			// get last element
-			var v = v[v.length-1];
-			if(v && typeof v.value != "undefined" && v.value != ''){
-				r.name = v.value
-				if(c && c.length > 0){
-					var c = c[c.length-1];
-					//console.log(c.value);
-					if(c.value != "undefined"){
-						r.content = c.value;
-					}
-				}
-				sampleSet.insert(r);
-			}else{
-				alert("Must at least have a name");
-			}
-		}else{
-			console.log('add record error');
-		}
-	};
-};
+
 Template.innerSurface.destroyed = function(){
 	// step one figure out surface..
 	var check =  _.findWhere(theSurfaces,{ _id : this.data._id });
 	if(check){
-		check.setContent('');
-		check.setProperties({display:"none"});
-		check._invisible = true;
-		check.visible = false;
-		// to make sure it is no longer clickable
-		check.on('click',function(){return null;});
-		check = null;
 		Template.multiSurface.rendered();
 		// word on the street you only hide disabled surfaces :/
 	}
 };
 Template.innerSurface.created = function(){
-	//console.log(this);
 	if(typeof theSurfaces != "undefined")
 		var check =  _.findWhere(theSurfaces,{ _id : this.data._id });
 	else
 		var check = false;
 	// attempt to clear out grid?
 	if(!check){
-		var s = new Surface({
-					size : [undefined,undefined],
-					properties : {color:"red",backgroundColor:"black"},
-					content : this.data.name
-				}
-				);
+		var s = makeInnerSurface(this.data._id,this.data.name,this.data.content);
 		if(s){
-			// storing these here because they lose scope in the on function
-			var theContent = this.data.content, theName = this.data.name, theId = this.data._id;
-			// may need to make one for id
-			s.on('click', function() {
-				// check for id.. force it to be a string just in case.. 
-				// most session vars are strings
-				if(!Session.equals("gridSelect",theId)){
-					Session.set("gridSelect",theId);
-					// add a link to allow for item deletion?
-					this.setContent(theContent + '<button onClick="sampleSet.remove({_id :\''+ theId +'\'});">Remove</button>');
-					
-					// background color check ...
-					if(this.properties.backgroundColor != "orange")
-						this.setProperties({backgroundColor:"orange",color:"white"});
-					else{
-						this.setContent(theName);
-						this.setProperties({backgroundColor:"black",color:"red"});
-					}
-				}else{
-					// restore 'defaults somehow...'
-					this.setContent(theName);
-					this.setProperties({backgroundColor:"black",color:"red"});
-					// clear the currently selected grid
-					Session.set("gridSelect",undefined);
-				}
-			});
-			// push surface for multiSurface.rendered
 			s._id = this.data._id;
 			if(typeof theSurfaces == "undefined"){
 				theSurfaces = [];
 			}
 			theSurfaces.push(s);
-			Template.multiSurface.rendered();
-			//add to context here too?
 		}else{
 			console.log('could not create surface');
 		}
